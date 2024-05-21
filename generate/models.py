ced60822b3cb2e860f73f618e1203aa931366708
Wyler Zahm <w3h3z3@gmail.com>
2024-05-21 13:40:11 -0400

    add basic question generation models

--- generate/models.py
+++ generate/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from ingest.models import FileModel
 
-# Create your models here.
+generate_execiton_types = (
+    ('MCQ', 'Multiple Choice Questions'),
+    ('FRQ', 'Free Response Questions'),
+)
+
+class GenerateExecutionModel(models.Model):
+    id = models.AutoField(primary_key=True) 
+    file_model_instance = models.ForeignKey(FileModel, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    generation_type = models.CharField(max_length=3, choices=generate_execiton_types)
+
+
+class GeneratedQuestionsModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    generate_execution_instance = models.ForeignKey(GenerateExecutionModel, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    answer_text = models.JSONField(default=dict)
+    correct_answer = models.JSONField(default=dict)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
